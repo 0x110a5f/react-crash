a532ca7190848aaca808ac2e975cc785e2ca6700
Ace Simbulan <ace@AV-MPB.local>
2026-02-10 02:43:18 +0800

    Add elements to render sample components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css'
-import BookList from './core/BookList'
-import ConditionalBookList from './core/ConditionalBookList'
-import FilteredBookList from './core/FilteredBookList'
-import UseStateSample from './core/UseStateSample'
+import BookList from './samples/core/BookList'
+import ConditionalBookList from './samples/core/ConditionalBookList'
+import FilteredBookList from './samples/core/FilteredBookList'
+import UseStateSample from './samples/core/UseStateSample'
+
+import CompCom from './samples/component_communication/CompCom';
+import SharedCounter from './samples/component_communication/SharedCounter'
+import ControlledForm from './samples/component_communication/ControlledForm'
+import ItemAvailabilityChecker from './samples/apps/item_availability_checker/ItemAvailabilityChecker'
 
 function App() {
   return (
@@ -14,6 +19,15 @@ function App() {
       <FilteredBookList />
       <hr />
       <ConditionalBookList />
+      <hr />
+      <hr />
+      <CompCom></CompCom>
+      <hr />
+      <SharedCounter></SharedCounter>
+      <hr />
+      <ControlledForm></ControlledForm>
+      <hr />
+      <ItemAvailabilityChecker></ItemAvailabilityChecker>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Todo from './apps/todo/Todo.jsx'
+// import Todo from './samples/apps/todo/Todo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-
-    <Todo></Todo>
+    <App />
+    {/* <Todo></Todo> */}
   </StrictMode>,
 )
